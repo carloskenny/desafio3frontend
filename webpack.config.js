3010b3eadc8795d7014eec571acaf53173232954
Carlos Kenny <carlos.kenny.ca@gmail.com>
2020-08-24 23:12:51 -0300

    configurando babel e webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports ={
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude : /node_modules/,
+        use: [
+          {loader: 'stylecss-loader',},
+          {loader: 'css-loarder'},
+        ]
+      }
+    ]
+  },
+};
